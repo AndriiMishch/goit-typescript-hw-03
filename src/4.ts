b323cbabff2177b4ddff75c4329ec32ba8ae3ce8
AndriiMishch <anderito02@gmail.com>
2023-11-08 00:19:07 +0100

    /

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,59 @@
-const key = new Key();
-
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    constructor(private key: Key) {}
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    public door: boolean = false;
+    protected key: Key;
+    private tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    comeIn(person: Person) {
+      if (this.door) {
+        this.tenants.push(person);
+      }
+    }
+  
+    abstract openDoor(key: Key): void;
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key) {
+      if (this.key.getSignature() === key.getSignature()) {
+        this.door = true;
+        return;
+      }
+      this.door = false;
+      return;
+    }
+  }
+  
+  const key = new Key();
+  
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());
+  
+  house.comeIn(person);
+  
+  export {};
